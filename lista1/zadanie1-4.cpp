8a1d42eaaaf2f33c8580f0d33d78099ddf8fd10b
mpolec <polec0@wp.pl>
2019-10-04 20:44:07 +0200

    Disabled cursor

--- lista1/zadanie1-4.cpp
+++ lista1/zadanie1-4.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 
-void test_coursor()
-{
-    cout << "\x1b[2J"; 
-    cout << "\x1b[1;0H ** \x1b[1;11H **";
-    cout << "\x1b[2;0H * \x1b[2;1H * \x1b[2;4H ** \x1b[2;8H ** \x1b[2;12H *";
-    cout << "\x1b[3;0H * \x1b[3;6H ** \x1b[3;12H *";
-    cout << "\x1b[4;0H * \x1b[4;12H *" << endl;
-}
+// void test_cursor()
+// {
+//     cout << "\x1b[2J"; 
+//     cout << "\x1b[1;0H ** \x1b[1;11H **";
+//     cout << "\x1b[2;0H * \x1b[2;1H * \x1b[2;4H ** \x1b[2;8H ** \x1b[2;12H *";
+//     cout << "\x1b[3;0H * \x1b[3;6H ** \x1b[3;12H *";
+//     cout << "\x1b[4;0H * \x1b[4;12H *" << endl;
+// }
 
 
 int main()
