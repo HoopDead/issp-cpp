33c9a319645b904953449c52bbc9e9f5a8a0b92a
Mateusz Połeć <polec0@wp.pl>
2019-10-09 09:15:54 +0200

    Reworked zadanie2-4.cpp

--- lista2/zadanie2-4.cpp
+++ lista2/zadanie2-4.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
+    cout << "\t-5\t-4\t-3\t-2\t-1\t0\t1\t2\t3\t4\t5" << endl;
     for (int i = -5; i < 6; i++)
     {
+        cout << i << "\t";
         for (int j = -5; j < 6; j++)
         {
-            cout << i << " * " << j << " = " << i*j << endl;
+            cout << i*j << "\t";
         }
-        cout << "==============" << endl;
+        cout << endl;
     }
+    cout << endl;
     return 0;
 }
--- lista2/zadanie2-7.cpp
+++ lista2/zadanie2-7.cpp
@@ -11,10 +11,11 @@ int main()
     double angle, result, resultroot;
     ofstream wyniki;
     wyniki.open("wyniki.dat", ios::out);
+    cout << "Kat\t" << "cos(x)\t\t" << "cos^2(x)" << endl;
     for(int i = 0; i < 361; i++)
     {
-        result = cos(i * PI / 180.0);
-        resultroot = pow(cos(i * PI / 180.0), 2);
+        result = cos(i * M_PI / 180.0);
+        resultroot = pow(cos(i * M_PI / 180.0), 2);
         cout << i << "\t" << result << "\t" << resultroot << endl;
         wyniki << i << "	" << result << "	" << resultroot << endl;
     }
